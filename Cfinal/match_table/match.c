53cf42927b2e8ad8245279b9b397bc2defa094d3
Joy-Zhe <67416165+Joy-Zhe@users.noreply.github.com>
2022-05-26 00:25:30 +0800

    ver0.1
    现在可以正常使用

--- Cfinal/match_table/match.c
+++ Cfinal/match_table/match.c
@@ -4,60 +4,66 @@
 #include<stdlib.h>
 
 struct token{
-    char *ch/*[10000]*/;//储存这个token的具体内容比如“void swap(){int a = 1;........................}”
+    char ch[10000];//储存这个token的具体内容比如“void swap(){int a = 1;........................}”
     char doc[100];//储存这个token所在文件的文件名比如“librarysystem.c”
     int startline;//这个token的第一行的行号
     int endline;//这个token最后一行的行号
 }Mtoken[100000];
 
-
-
 typedef struct s_Token{//链表表头存放当前串的信息（因为无需记录第一个出现的子串），而每次遍历都是从头开始的，故可将表头作为索引
     int index;//存放找到的
-	char *contain;// 仅在存放表头时有效，即s_Token[i]->contain
+	char contain[10001];// 仅在存放表头时有效，即s_Token[i]->contain
     struct s_Token *next;
 }LinkList;
 LinkList *M[100001];
 int M_book = 0;
 
-LinkList *map_create(/*int n, */char *subToken);//遇到未加入映射表的subToken，建立新的键
+LinkList *map_create(/*int n, */char *subToken, int n);//遇到未加入映射表的subToken，建立新的键, n为第一次遇到时的Token编号，后续编号从M[]->next开始
 void map_add(LinkList *list, int n/*, char *subToken*/);
 void map_key(LinkList *list[], char *subToken/*, char *Token[]*/, int start_pos, int sublen);//构建映射表
 int judge(LinkList *M_now, int n);//判断该subToken所在的Token是否已经在映射表中被记录过
 
 int /*mapping_*/main (){//之后改成映射函数
 	int cnt_token = 2, i = 1;
-	Mtoken[1].ch = "ab";
-    Mtoken[2].ch = "dnab";
+	//Mtoken[1].ch = "abababab";
+    //Mtoken[2].ch = "dnabcba";
 	char subToken[100001];//存放当次子串
     //char Token[100001][100001];//母串 //改为使用新的结构体数组
-    int index_Token = 1, sublen = 1, pos = 0, index_subToken = 1;
+    int index_Token = 1, sublen = 2, pos = 0, index_subToken = 1;
     //取出键
 	for(i = 1; i <= cnt_token; i++){
 		pos = 0;
-		for(int j = pos; j <= strlen(Mtoken[i].ch) - sublen + 1; j++){
+		for(int j = pos; j <= strlen(Mtoken[i].ch) - sublen; j++){
 			for(int k = 0; k < sublen; k++){
 				subToken[k] = Mtoken[i].ch[k + j];
 			}
 			subToken[sublen] = '\0';
 			map_key(M, subToken, i, sublen);
 		}
-	}LinkList *p;
-	//测试输出
-	for(int j = 0;j <= M_book; j++){
-		printf("%s",M[j]->contain);
-		for(p = M[j]; p; p = p->next){
-			printf("%d\n", p->index);
+	}
+	LinkList *p;
+	/*测试输出
+	for(int j = 0;j < M_book; j++){
+		printf("%s:", M[j]->contain);
+		for(p = M[j]->next; p; p = p->next){
+			printf("%d ", p->index);
 		}
-	}//
+		printf("\n");
+	}*/
 
     //构建对应值
 }
 
-LinkList *map_create(/*int n, */char *subToken){
+LinkList *map_create(/*int n, */char *subToken, int n){
 	LinkList *head;//定义头节点
+	int i = 0;
 	head = (LinkList*)malloc(sizeof(LinkList));//分配地址
-	head->contain = subToken; //存放当前表中不存在的新键
+	while(*(subToken + i)){
+		head->contain[i] = *(subToken + i);
+		i++;
+	}//存放当前表中不存在的新键
+	head->contain[i] = '\0';
+	head->index = n;
 	head->next = NULL;//结束创建
 	return head;
 }
@@ -86,9 +92,9 @@ void map_key(LinkList *list[], char *subToken/*, char *Token[]*/, int n, int sub
 	// 		now = 0;
 	// 	pos++;
 	// }
-	while (index <= M_book/*list[index]->head[0]*/){
+	while (index < M_book/*list[index]->head[0]*/){
 		if(M_book == 0){
-			M[M_book] = map_create(subToken);
+			M[M_book] = map_create(subToken, n);
 			M_book++;
 			break;
 		}
@@ -97,10 +103,14 @@ void map_key(LinkList *list[], char *subToken/*, char *Token[]*/, int n, int sub
 			flag = 1;
 			break;
 		}	//链表
+		if(strcmp(subToken, list[index]->contain) == 0 && judge(list[index], n)){ //在内且在该Token内首次出现， 节点后移记录
+			flag = 1;
+			break;
+		}
 		index++;
 	}
 	if(flag == 0){ // 不在内， 创建新键
-		M[M_book] = map_create(subToken);
+		M[M_book] = map_create(subToken, n);
 		M_book++;
 	}
 }
